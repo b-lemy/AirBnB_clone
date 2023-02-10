d7803e9639265d89c6785310c213101aef94eaf1
b-lemy <lemabrian1234@gmail.com>
2023-02-10 05:41:44 -0800

    console

--- console.py
+++ console.py
@@ -1,13 +1,29 @@
 #!/usr/bin/python3
 
 """
-  My console file 
+  My console file
 """
 
 import cmd
 
-class HBNBCommand(cmd.Cmd):
 
+class HBNBCommand(cmd.Cmd):
+    """
+    Command Line Interpreter
+    """
+    prompt = '(hbnb)'
+    def do_quit(self, line):
+        """
+          quit: quit USAGE: Command to quit the program
+        """
+        return True
+
+    def do_EOF(self, line):
+        """
+           function to handle EOF
+        """
+        print()
+        return True
 
 
 if __name__ == '__main__':

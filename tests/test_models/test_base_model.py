f2fe6482ad14b2eb2104b65726ca46c282dca8dd
b-lemy <lemabrian1234@gmail.com>
2023-02-12 04:32:08 -0800

    test

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -3,3 +3,14 @@
 """
    base model test case
 """
+import os
+import models
+import unittest
+from datetime import datetime
+from time import sleep
+from models.base_model import BaseModel
+
+
+class TestBaseModel_instantiation(unittest.TestCase):
+    """Unittests for testing instantiation of the BaseModel class."""
+print ("first test")

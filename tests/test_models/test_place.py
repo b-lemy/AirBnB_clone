eaab412d7117fba70b624439ad79076f286c55e8
b-lemy <lemabrian1234@gmail.com>
2023-02-13 06:04:08 -0800

    tests

--- tests/test_models/test_place.py
+++ tests/test_models/test_place.py
@@ -6,5 +6,22 @@ Tests for the Place Model
 
 import unittest
 import datetime
-
 from models.place import Place
+
+
+class TestPlace(unittest.TestCase):
+    """Test the Reviews"""
+    def setUp(self):
+        """An instance just for tests"""
+        self.a1 = Place()
+        self.a2 = Place()
+
+    def test_instances(self):
+        """Test the instances"""
+        self.assertTrue(isinstance(self.a1, Place))
+        self.assertTrue(isinstance(self.a2, Place))
+        self.assertTrue(hasattr(self.a1, "name"))
+
+
+if __name__ == '__main__':
+    unittest.main()
--- tests/test_models/test_review.py
+++ tests/test_models/test_review.py
@@ -12,3 +12,17 @@ from models.review import Review
 
 class TestReview(unittest.TestCase):
     """Test the Reviews"""
+    def setUp(self):
+        """An instance just for tests"""
+        self.a1 = Review()
+        self.a2 = Review()
+
+    def test_instances(self):
+        """Test the instances"""
+        self.assertTrue(isinstance(self.a1, Review))
+        self.assertTrue(isinstance(self.a2, Review))
+        self.assertTrue(hasattr(self.a1, "text"))
+
+
+if __name__ == '__main__':
+    unittest.main()
